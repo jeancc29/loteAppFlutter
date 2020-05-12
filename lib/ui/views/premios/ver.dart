9f7fc36bef2309805aa077c1575f03e053cfbf62
Jean Contreras <jeancon29@gmail.com>
2020-05-12 17:07:30 -0400

    Copia de seguridad

--- lib/ui/views/premios/ver.dart
+++ lib/ui/views/premios/ver.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:loterias/core/classes/utils.dart';
+import 'package:loterias/core/models/loterias.dart';
 import 'package:loterias/core/services/premiosservice.dart';
+import 'package:rxdart/rxdart.dart';
 
 class VerPremios extends StatefulWidget {
   @override
@@ -11,18 +15,90 @@ class VerPremios extends StatefulWidget {
 
 class _VerPremiosState extends State<VerPremios> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
+  StreamController<List<Loteria>> _streamControllerLoteria;
+  List<Loteria> listaLoteria = List();
   bool _cargando = false;
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    _streamControllerLoteria = BehaviorSubject();
+    _init();
+    super.initState();
+  }
 
   _init() async {
   try{
     setState(() => _cargando = true);
-    var datos = await PremiosService.getLoterias(scaffoldKey: _scaffoldKey);
+    var listaLoteria = await PremiosService.getLoterias(scaffoldKey: _scaffoldKey);
+    print("LIsta loteria: ${listaLoteria.length}");
+    _streamControllerLoteria.add(listaLoteria);
     setState(() => _cargando = false);
   }on Exception catch(e){
     setState(() => _cargando = false);
   }
 }
+
+Padding _card(Loteria loteria){
+  print("dentro _card: ${loteria.primera.isEmpty}");
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Card(
+      child: Column(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("${loteria.descripcion}", style: TextStyle(fontSize: 20)),
+            )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: Wrap(
+              spacing: 5,
+              children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  color: (loteria.primera.isNotEmpty) ? Colors.blue : Colors.grey,
+                  child: Center(
+                    child: Text("${(loteria.primera.isNotEmpty) ? loteria.primera : '-'}"),
+                  ),
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  color: (loteria.segunda.isNotEmpty) ? Colors.blue : Colors.grey,
+                  child: Center(
+                    child: Text("${(loteria.segunda.isNotEmpty) ? loteria.segunda : '-'}"),
+                  ),
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  color: (loteria.tercera.isNotEmpty) ? Colors.blue : Colors.grey,
+                  child: Center(
+                    child: Text("${(loteria.tercera.isNotEmpty) ? loteria.tercera : '-'}"),
+                  ),
+                ),
+              ),
+            ],),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -35,41 +111,16 @@ class _VerPremiosState extends State<VerPremios> {
       ),
       home: Scaffold(
         body: SafeArea(
-          child: ListView(
-            children: <Widget>[
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Text("Nacional"),
-                    Wrap(children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        child: Container(
-                          width: 50,
-                          height: 50,
-                          color: Colors.blue,
-                          child: Center(
-                            child: Text("20"),
-                          ),
-                        ),
-                      ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        child: Container(
-                          width: 50,
-                          height: 50,
-                          color: Colors.blue,
-                          child: Center(
-                            child: Text("20"),
-                          ),
-                        ),
-                      ),
-                    ],)
-                  ],
-                ),
-              ),
-
-            ],
+          child: StreamBuilder<List<Loteria>>(
+            stream: _streamControllerLoteria.stream,
+            builder: (context, snapshot) {
+              if(snapshot.hasData){
+                return ListView(
+                  children: snapshot.data.map<Padding>((l) => _card(l)).toList(),
+                );
+              }
+              return Center(child: Text("No hay datos"));
+            }
           ),
         ),
       ),
